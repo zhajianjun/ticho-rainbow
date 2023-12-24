38e50ded8ffb44c843725288bad863e02d6d5cbe
zhajianjun <1019319473@qq.com>
2023-12-24 18:06:53 +0800

    fix: [front]升级后的bug修复

--- ticho-intranet-admin/src/views/sys/about/index.vue
+++ ticho-intranet-admin/src/views/sys/about/index.vue
@@ -18,12 +18,12 @@
   import { h } from 'vue';
   import { Tag } from 'ant-design-vue';
   import { PageWrapper } from '@/components/Page';
-  import { Description, DescItem, useDescription } from '@/components/Description/index';
+  import { Description, DescItem, useDescription } from '@/components/Description';
   import { GITHUB_URL, SITE_URL, DOC_URL } from '@/settings/siteSetting';
 
   const { pkg, lastBuildTime } = __APP_INFO__;
 
-  const { dependencies, devDependencies, name, version } = pkg;
+  const { dependencies, devDependencies, version } = pkg;
 
   const schema: DescItem[] = [];
   const devSchema: DescItem[] = [];
