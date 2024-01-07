1fd1b9ff949cd264ca06699adabd23ab65b7de1e
zhajianjun <1019319473@qq.com>
2024-01-07 16:54:30 +0800

    style: [front]登录页面默认用户密码去除

--- ticho-intranet-admin/src/views/sys/login/LoginForm.vue
+++ ticho-intranet-admin/src/views/sys/login/LoginForm.vue
@@ -119,8 +119,8 @@
   const rememberMe = ref(false);
 
   const formData = reactive({
-    account: 'admin',
-    password: '123456',
+    account: null,
+    password: null,
   });
 
   const { validForm } = useFormValid(formRef);
