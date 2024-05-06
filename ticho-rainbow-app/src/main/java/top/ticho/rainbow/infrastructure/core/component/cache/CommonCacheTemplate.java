04a710ef9f619b2555dd11c1c93f0d18873247bd
zhajianjun <1019319473@qq.com>
2024-05-06 14:50:15 +0800

    feat: [backend] 公共缓存模板工具优化

--- ticho-rainbow-app/src/main/java/top/ticho/rainbow/infrastructure/core/component/cache/CommonCacheTemplate.java
+++ ticho-rainbow-app/src/main/java/top/ticho/rainbow/infrastructure/core/component/cache/CommonCacheTemplate.java
@@ -13,10 +13,14 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Executor;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
+ * 公共缓存模板工具
+ *
  * @author zhajianjun
  * @date 2024-04-28 08:43
  */
@@ -27,10 +31,21 @@ public class CommonCacheTemplate<T> {
     private final Cache<String, T> cache;
 
     public CommonCacheTemplate(Function<T, Long> expireFunction) {
-        this(null, expireFunction, null);
+        this(null, expireFunction, null, null);
     }
 
-    public CommonCacheTemplate(Function<String, T> function, Function<T, Long> expireFunction, Executor executor) {
+    /**
+     * @param cacheLoadFunction   缓存加载逻辑
+     * @param cacheExpireFunction 缓存过期逻辑
+     * @param cacheRemoveConsumer 缓存移除处理
+     * @param executor            线程池
+     */
+    public CommonCacheTemplate(
+        Function<String, T> cacheLoadFunction,
+        Function<T, Long> cacheExpireFunction,
+        BiConsumer<String, T> cacheRemoveConsumer,
+        Executor executor
+    ) {
         // @formatter:off
         Caffeine<String, T>  stringTCaffeine = Caffeine.newBuilder()
             //初始容量
@@ -40,7 +55,7 @@ public class CommonCacheTemplate<T> {
             // 打开统计功能
             .recordStats()
             // 设置自定义过期
-            .expireAfter(new DefaultExpiry<>(expireFunction))
+            .expireAfter(new DefaultExpiry<>(cacheExpireFunction))
             // 设置固定过期 失效后同步加载缓存，阻塞机制获取缓存
             //.expireAfterWrite(Duration.ofMinutes(1))
             // 设置固定过期 失效后异步加载，其它线程任然获取旧值
@@ -48,13 +63,13 @@ public class CommonCacheTemplate<T> {
             // 缓存写入删除回调 同步
             //.writer(new DefaultCacheWriter())
             // 缓存移除监听 异步
-            .removalListener(new DefaultRemovalListener<>());
-        if (executor != null) {
+            .removalListener(new DefaultRemovalListener<>(cacheRemoveConsumer));
+        if (Objects.nonNull(executor)) {
             // 异步线程池
             stringTCaffeine.executor(executor);
         }
-        if (function != null) {
-            cache = stringTCaffeine.build(new DefaultCacheLoader<>(function));
+        if (Objects.nonNull(cacheLoadFunction)) {
+            cache = stringTCaffeine.build(new DefaultCacheLoader<>(cacheLoadFunction));
         } else {
             cache = stringTCaffeine.build();
         }
@@ -80,10 +95,10 @@ public class CommonCacheTemplate<T> {
 
     static class DefaultExpiry<T> implements Expiry<String, T> {
         /** 过期时间转换逻辑 **/
-        private final Function<T, Long> expireFunction;
+        private final Function<T, Long> cacheExpireFunction;
 
-        public DefaultExpiry(Function<T, Long> expireFunction) {
-            this.expireFunction = expireFunction;
+        public DefaultExpiry(Function<T, Long> cacheExpireFunction) {
+            this.cacheExpireFunction = cacheExpireFunction;
         }
 
         /**
@@ -96,7 +111,7 @@ public class CommonCacheTemplate<T> {
          */
         @Override
         public long expireAfterCreate(@NonNull String key, @NonNull T value, long currentTime) {
-            Long expire = expireFunction.apply(value);
+            Long expire = cacheExpireFunction.apply(value);
             return Duration.ofMillis(expire).toNanos();
         }
 
@@ -111,7 +126,7 @@ public class CommonCacheTemplate<T> {
          */
         @Override
         public long expireAfterUpdate(@NonNull String key, @NonNull T value, long currentTime, @NonNegative long currentDuration) {
-            Long expire = expireFunction.apply(value);
+            Long expire = cacheExpireFunction.apply(value);
             return Duration.ofMillis(expire).toNanos();
         }
 
@@ -126,7 +141,7 @@ public class CommonCacheTemplate<T> {
          */
         @Override
         public long expireAfterRead(@NonNull String key, @NonNull T value, long currentTime, @NonNegative long currentDuration) {
-            Long expire = expireFunction.apply(value);
+            Long expire = cacheExpireFunction.apply(value);
             return Duration.ofMillis(expire).toNanos();
         }
     }
@@ -155,12 +170,21 @@ public class CommonCacheTemplate<T> {
      * </p>
      */
     static class DefaultRemovalListener<T> implements RemovalListener<String, T> {
+        /** 缓存移除处理 */
+        private final BiConsumer<String, T> consumer;
+
+        public DefaultRemovalListener(BiConsumer<String, T> consumer) {
+            if (Objects.isNull(consumer)) {
+                consumer = (x, y) -> {};
+            }
+            this.consumer = consumer;
+        }
 
         @Override
         public void onRemoval(String key, T value, @NonNull RemovalCause cause) {
             log.info("缓存移除监听, 移除的key = {}, value = {}, cause = {}", key, value, cause);
+            consumer.accept(key, value);
         }
-
     }
 
 }
