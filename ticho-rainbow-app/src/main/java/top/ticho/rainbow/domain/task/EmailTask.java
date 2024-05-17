5923973e0cfdd3a236e9631df065e7f2b43e56b9
zhajianjun <1019319473@qq.com>
2024-05-17 17:28:13 +0800

    feat: 邮件发送定时任务添加

---
+++ ticho-rainbow-app/src/main/java/top/ticho/rainbow/domain/task/EmailTask.java
@@ -0,0 +1,42 @@
+package top.ticho.rainbow.domain.task;
+
+import org.quartz.JobExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import top.ticho.boot.mail.component.MailContent;
+import top.ticho.boot.view.util.Assert;
+import top.ticho.boot.web.util.valid.ValidUtil;
+import top.ticho.rainbow.domain.repository.EmailRepository;
+import top.ticho.rainbow.infrastructure.core.component.AbstracTask;
+import top.ticho.rainbow.interfaces.dto.MailContentDTO;
+
+/**
+ * 邮件任务
+ *
+ * @author zhajianjun
+ * @date 2024-05-17 16:33
+ */
+@Component
+public class EmailTask extends AbstracTask<MailContentDTO> {
+
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @Override
+    public void run(JobExecutionContext context) {
+        MailContentDTO taskParam = getTaskParam(context);
+        ValidUtil.valid(taskParam);
+        MailContent mailContent = new MailContent();
+        mailContent.setTo(taskParam.getTo());
+        mailContent.setSubject(taskParam.getSubject());
+        mailContent.setContent(taskParam.getContent());
+        mailContent.setCc(taskParam.getCc());
+        boolean sendMail = emailRepository.sendMail(mailContent);
+        Assert.isTrue(sendMail, "发送邮件失败");
+    }
+
+    @Override
+    public Class<MailContentDTO> getParamClass() {
+        return MailContentDTO.class;
+    }
+}
